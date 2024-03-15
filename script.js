ba0e059fee1c0ed92910190f414f85287178855c
rela7e <pdarcy@riseup.net>
2024-03-15 09:33:20 -0400

    add calculate score function

--- script.js
+++ script.js
@@ -1,80 +1,56 @@
-const player = {
-    cards: [],
-    handValue: 0,
-}
-const dealer = {
-    cards: [],
-    handValue: 0,
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+let deck = [];
+let playerHand = [];
+let dealerHand = [];
+
+function calculateScore(hand) {
+    let score = 0;
+    let numAces = 0;
+
+    for (let i = 0; i < hand.length; i++) {
+        const card = hand[i].value;
+        if (card === "J" || card === "Q" || card === "K") {
+            score += 10;
+        } else if (card === "A") {
+            numAces++;
+            score += 11;
+        } else {
+            score += parseInt(card);
+        }
+    }
+
+    // Adjust score for multiple Aces
+    while (score > 21 && numAces > 0) {
+        score -= 10;
+        numAces--;
+    }
+
+    // Return "Bust" if score exceeds 21
+    if (score > 21) {
+        return "Bust";
+    }
+
+    return score;
 }
 
-var chipCount = 100
-var betAmount = 0
-
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-const suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
-const deck = []
-const cardValues = {
-    "2" : 2,
-    "3" : 3,
-    "4" : 4,
-    "5" : 5,
-    "6" : 6,
-    "7" : 7,
-    "8" : 8,
-    "9" : 9,
-    "10" : 10,
-    "J" : 10,
-    "Q" : 10,
-    "K" : 10,
-    "A" : 11,
-}
-
-function buildDeck(){
-    for (let s = 0; s < suits.length; s++){
-        for (let v = 0; v < values.length; v++){
+function buildDeck() {
+    for (let s = 0; s < suits.length; s++) {
+        for (let v = 0; v < values.length; v++) {
             const value = values[v];
             const suit = suits[s];
-            deck.push({value , suit});
+            deck.push({ value, suit });
         }
     }
     return deck;
 }
 
-function shuffleDeck(arr){
-    for (var i = arr.length - 1; i > 0; i--){
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = arr[i];
+function shuffleDeck(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    return arr;
 }
-
-function dealPlayer(){
-    var dealt = (deck.pop());
-    player.cards.push(dealt)
-
-}
-
-function dealDealer(){
-    var dealt = (deck.pop());
-    dealer.cards.push(dealt);
-}
-
-function checkScore(){
-
-}
-
-function startGame() {
-    player.cards.splice(0 , player.cards.length);
-    dealer.cards.splice(0 , dealer.cards.length);
-    buildDeck();
-    shuffleDeck(deck);
-    dealPlayer();
-    dealPlayer();
-    dealDealer();
-    dealDealer();
-    
-}
-
-
-
